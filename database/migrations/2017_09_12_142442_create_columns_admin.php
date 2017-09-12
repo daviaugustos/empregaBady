c079f1c08edd2f007d8f39cde51d32508911ea07
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-09-12 11:33:09 -0300

    Criação das colunas da tabela admins

---
+++ database/migrations/2017_09_12_142442_create_columns_admin.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColumnsAdmin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admins', function (BluePrint $table){ 
+            $table->string('nome');
+            $table->string('email');
+            $table->string('senha');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admins', function (BluePrint $table){ 
+            $table->dropColumns('nome');
+            $table->dropColumns('email');
+            $table->dropColumns('senha');
+        });
+    }
+}
