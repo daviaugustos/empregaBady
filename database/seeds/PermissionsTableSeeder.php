458a522ace53b0dd1b7133b65c6c373cff7a6e2c
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-10-20 09:57:11 -0200

    Criação do seed da tabela permission

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('permissions')->insert([
+            'name' => 'Administer roles & permissions',
+            'guard_name' => 'web',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+        
+        DB::table('permissions')->insert([
+            'name' => 'Criar vaga',
+            'guard_name' => 'web',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+        
+        DB::table('permissions')->insert([
+            'name' => 'Gerenciar vagas',
+            'guard_name' => 'web',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
