fd686643db8d0047fd54de7d5e84ab03a48d75c4
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-10-23 09:33:18 -0200

    Implementação do seeder de usuário

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+            'name' => 'Desenvolvedor',
+            'email' => 'sistemas@badybassitt.com.br',
+            'password' => '123qweasd',
+            'telefone' => '(17) 9999-9999',
+            'celular' => '(17) 99999-9999',
+            'enderecoCidade' => '1',
+            'enderecoEstado' => '1',
+            'contactable_id' => '0',
+            'contactable_type' => 'App\Candidato',
+        ]);
+        $user->assignRole('AdminFull');
     }
 }
