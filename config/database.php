6583ddc3df8da45355d07536e83896956c4d6e9a
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-10-30 15:58:08 -0200

    Alterações pra conexão do banco em produção

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql857.umbler.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bd_empregaBady'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' => env('DB_DATABASE', 'bd_empregabady'),
+            'username' => env('DB_USERNAME', 'adminprefeitura'),
+            'password' => env('DB_PASSWORD', 'W98qbAmU(9/'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
