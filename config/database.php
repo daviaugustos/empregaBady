270d2ea08e7b6d1f7de130e26f6b7bd1c77c426c
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-10-30 10:48:32 -0200

    Alteração dos arquivos de conexão com o banco de dados

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'mysql857.umbler.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'bd_empregabady'),
             'username' => env('DB_USERNAME', 'adminprefeitura'),
