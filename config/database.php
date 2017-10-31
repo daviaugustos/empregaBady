37c07c55d0bb72732ba748ac37c3b4116caecb49
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-10-31 11:53:24 -0200

    Correção do nome do banco de dados

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bd_empregaBady'),
+            'database' => env('DB_DATABASE', 'bd_empregabady'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'charset' => 'utf8mb4',
